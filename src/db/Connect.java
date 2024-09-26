8ad552424e565ef90854d011cda8677147238413
ArthurRudson <arthurrudson@gmail.com>
2024-09-26 13:09:31 -0300

    findById implementation

--- src/db/Connect.java
+++ src/db/Connect.java
@@ -6,6 +6,7 @@ import java.sql.SQLException;
 
 public class Connect {
 
+	
 	static final String URL = "jdbc:postgresql://localhost:5432/postgres"; // incica o caminho do banco de dados
 	static final String USER = "postgres"; // aqui vai o nome usuario que vc quer acessar
 	static final String PASS = "26841397a@"; // aqui a senha do seu banco
--- src/model/dao/DaoFactory.java
+++ src/model/dao/DaoFactory.java
@@ -1,11 +1,14 @@
 package model.dao;
 
+import java.sql.SQLException;
+
+import db.Connect;
 import model.dao.impl.SellerDaoJDBC;
 
 public class DaoFactory {
 	
-	public static SellerDao createSellerDao() {
-		return new SellerDaoJDBC();
+	public static SellerDao createSellerDao() throws ClassNotFoundException, SQLException {
+		return new SellerDaoJDBC(Connect.criarConexao());
 	}
 
 }
