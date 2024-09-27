7fa56306478bc98adec878df7a13d155a6bd0c3f
ArthurRudson <arthurrudson@gmail.com>
2024-09-27 16:06:48 -0300

    DepartmentDao implementation and test

--- src/model/dao/DaoFactory.java
+++ src/model/dao/DaoFactory.java
@@ -3,12 +3,17 @@ package model.dao;
 import java.sql.SQLException;
 
 import db.Connect;
+import model.dao.impl.DepartmentDaoJDBC;
 import model.dao.impl.SellerDaoJDBC;
 
 public class DaoFactory {
 	
 	public static SellerDao createSellerDao() throws ClassNotFoundException, SQLException {
 		return new SellerDaoJDBC(Connect.criarConexao());
+	}
+	
+	public static DepartmentDao createDepartmentDao() throws ClassNotFoundException, SQLException {
+		return new DepartmentDaoJDBC(Connect.criarConexao());
 	}
 
 }
